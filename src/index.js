08816c28feae92402b35a859f3df52fe0f0b5d64
mjadzevicius@gmail.com <mjadzevicius@gmail.com>
2020-03-24 23:31:24 +0200

    Added google fonts, created style.css file and added normalize.css

--- src/index.js
+++ src/index.js
@@ -2,5 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+import 'normalize.css';
+import './style.css';
+
 // eslint-disable-next-line react/jsx-filename-extension
 ReactDOM.render(<App />, document.getElementById('root'));
