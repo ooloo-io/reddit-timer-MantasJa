bb19b96cf4a2db990fe748c0eecef3e29d91c1c0
mjadzevicius@gmail.com <mjadzevicius@gmail.com>
2020-03-25 01:00:10 +0200

    Added react-router-dom package, created two new routes with temp components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import NotFound from './pages/NotFound';
+import Home from './pages/Home';
+import Search from './pages/Search';
 
 function App() {
   return (
-    <div>
-      App Placeholder
-    </div>
+    <>
+      <Router>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/search">Search</Link>
+          </li>
+        </ul>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/search" component={Search} />
+          <Route component={NotFound} />
+        </Switch>
+      </Router>
+    </>
   );
 }
 
